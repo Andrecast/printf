cc983588e4c2c5eaa38e177536834be85347b35f
Andrecast <acastrillonpuerta@gmail.com>
2021-03-16 17:39:57 -0500

    Pointer to the total acummulator

--- helper_printf.c
+++ helper_printf.c
@@ -1,21 +1,27 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
+ *helper_printf - Function that shows the lenght of format
+ *@format: string that contains all the message
+ *@struct_func: Structure of the functions that contains the specifiers
+ *@args: contains the variadic function arguments
+ *Return: return the total of characters to be printed
  */
-int helper_printf(const char *format, struct_form *, va_list args)
+int helper_printf(const char *format, struct_form struct_func[], va_list args)
 {
 	int i, j;
-    
+	int *ptr;
+	int acum_total = 0;
+
+	ptr = &acum_total;
+
 	i = 0;
 	while(format[i] != '\0' && format)
 	{
 		if (format[i] != '%' && format[i] != '\0')
 		{
 			_putchar(format[i]);
+			acum_total++;
 			i++;
 		}
 		if (format[i] == '\0')
@@ -24,16 +30,18 @@ int helper_printf(const char *format, struct_form *, va_list args)
 		if (format[i] == '%')
 		{
 			_putchar(format[i]);
+			acum_total++;
 		}
 		j = 0;
 		while(struct_func[j].option != NULL)
 		{
 			if (format[i] == struct_func[j].option[0])
 			{
-				struct_func[j].func(args);
+				struct_func[j].func(args, ptr);
 			}
 			j++;
 		}
 		i++;
 	}
+	return (*ptr);
 }
